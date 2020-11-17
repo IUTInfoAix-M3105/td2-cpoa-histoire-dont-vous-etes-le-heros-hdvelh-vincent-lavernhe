680196d707e249e9ccc64a35f245ecb1ae35bf9a
vincent-lavernhe <vincent.lavernhe@etu.univ-amu.fr>
2020-11-17 11:35:58 +0100

    Ajout des variables private Object = data et NodeMultiple

--- src/pracHDVELH/NodeMultiple.java
+++ src/pracHDVELH/NodeMultiple.java
@@ -15,7 +15,8 @@ public class NodeMultiple {
 	public static final int ERROR_STATUS_INDEX_OUT_OF_RANGE = -1;
 	public static final String ERROR_MSG_INDEX_OUT_OF_RANGE = "Index out of range";
 	public static int NODE_MAX_ARITY = 10;
-
+	private Object data;
+	private NodeMultiple[] daughters;
 	/* Overridden methods */
 	@Override
 	public String toString() {
