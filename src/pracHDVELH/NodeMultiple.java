41506b53730fb6e10d396cf213ee131c9cfc1aa7
vincent-lavernhe <vincent.lavernhe@etu.univ-amu.fr>
2020-11-17 11:33:32 +0100

    SetDaughter avec le message d'erreur

--- src/pracHDVELH/NodeMultiple.java
+++ src/pracHDVELH/NodeMultiple.java
@@ -51,7 +51,12 @@ public class NodeMultiple {
 	 */
 	public void setDaughter(NodeMultiple daughter, int i) {
 		/* TO BE COMPLETED */
+		if (i <0 || i > NODE_MAX_ARITY) {
+			ErrorNaiveHandler.abort(ERROR_MSG_INDEX_OUT_OF_RANGE + "0" + getClass() + ".setDaughters()");
+		}
+		daughters[i]=daughter;
 	}
+	
 
 	/**
 	 * @return all the daughters
